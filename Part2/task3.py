8786899b3e83db97c45b68637b27745df0d2b7c0
SOLOMONOVICH EYAL <EYALSOL@rafael.co.il>
2024-05-05 14:35:08 +0300

    finished Part2-Task3

---
+++ Part2/task3.py
@@ -0,0 +1,20 @@
+import math
+
+def is_sorted_polyndrom(word: str) -> bool:
+  for i in range(len(word)):  #Loop over the word
+
+    char_start = word[i]
+    char_end = word[len(word) - 1 - i]  #The mirrored char from the end
+
+    if char_start != char_end:  #The char is not the same as the mirrored char
+      return False
+
+    if i > 0 and word[i] < word[i - 1]:  #The chars aren't sorted
+      return False
+
+    if (i == len(word) - 1 - i) or (i + 1 > len(word) - i):  #Reached the middle or the next step will switch the chars
+      return True
+  return True
+
+
+print(is_sorted_polyndrom('אבגדגבא'))
